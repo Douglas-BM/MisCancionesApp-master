fc4dbba12c4ee7ae03dd9bb58b4726789282d5d8
DOUGLAS-BM <71328524@continental.edu.pe>
2021-05-26 09:15:00 -0500

    commit

---
+++ src/miscancionesapp/PruebaAlbun.py
@@ -0,0 +1,27 @@
+from src.miscancionesapp.modelo.cancion import Cancion
+from src.miscancionesapp.modelo.interprete import Interprete
+from src.miscancionesapp.modelo.album import Album, Medio
+from src.miscancionesapp.modelo.declarative_base import Session, engine, Base
+from src.miscancionesapp.logica.coleccion import Coleccion
+
+if __name__ == '__main__':
+    #Crea la BD
+    Base.metadata.create_all(engine)
+
+    #Abre la sesion
+    session = Session()
+    colecion=Coleccion()
+
+    if colecion.agregar_album("Incondicionales" , 1990 , "Son baladas" , Medio.CD ):
+        print("El album Se añadio  con exito")
+    else:
+        print ( "Ya se encuentra el album" )
+
+    if colecion.agregar_cancion ( "Amor" , 2 , 30 , "Prisioneros" ):
+        print ( "La canción Se añadio  con exito" )
+    else :
+        print ( "Ya se encuentra la canción" )
+    if colecion.agregar_interprete( "Juan" , "Canta autor" ):
+        print ( "El interprete Se añadio  con exito" )
+    else :
+        print ( "Ya se encuentra el interprete" )
